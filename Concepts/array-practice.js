adde98f5a72f529ca1db27fff1fd05435307566a
exclusiveabhi <imexclusiveabhi@gmail.com>
2025-01-27 22:26:29 +0530

    27/01/2025

---
+++ Concepts/array-practice.js
@@ -0,0 +1,47 @@
+// find the average marks
+
+// let marksArr = [85,97,44,37,76,60]
+// let totalVal = 0
+// for(let mark of marksArr){
+//     totalVal = totalVal + mark;
+// }
+// let averageMarks = totalVal/marksArr.length
+
+// console.log(averageMarks)
+
+
+//offer problem 
+
+// let originalArr = [250,645,300,900,50]
+
+// for(let i = 0; i<originalArr.length; i++){
+//     let offerPrice = originalArr[i]*10/100
+//     actualPrice = originalArr[i]-offerPrice
+//     originalArr[i]=actualPrice
+// }
+
+// console.log(originalArr)
+
+//array methods push
+// let foodArr = ["apple","mango","pineapple","graps"]
+// let newValue = "banana"
+// console.log(`Array before ${foodArr}`)
+// foodArr.push(newValue)
+// console.log(`Array after ${foodArr}`)
+
+//array methods pop
+
+// let foodArr = ["apple","mango","pineapple","graps"]
+// console.log(`Array before ${foodArr}`)
+// foodArr.pop()
+// console.log(`Array after ${foodArr}`)
+
+
+//shift method in array
+
+// let arr = [3,4,35,45,565,65,56,56,6]
+// console.log(`array before shifting ${arr}`)
+// let shiftedArr = arr.shift()
+// console.log(`array after shifted ${shiftedArr} and become orignal array is ${arr}`)
+
+
---
+++ Concepts/array.js
@@ -0,0 +1,32 @@
+//basics
+
+// let arr = [55,44,5,6,6,7,545,445,44]
+
+// console.log(arr[7])
+
+//replacing in  arr value
+
+// let arr = [55,44,5,6,6,7,545,445,44]
+// console.log(arr[4])
+// arr[4] = 1
+// console.log(arr[4])
+
+
+//print all array value using loop
+
+// let arr = [55,44,5,6,10,7,545,445,44]
+
+// for(let i = 0; i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+//for of loop to print all array items
+
+// let arr = [55,44,5,6,10,7,545,445,44]
+
+// for(let item of arr){   same work as for loop but better aproach !
+//     console.log(item)
+// }
+
+
+//_________________________________________________________________________________________________________________
