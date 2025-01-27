da774e9bd9f89e49763e2a6e511c66282b02cc03
exclusiveabhi <imexclusiveabhi@gmail.com>
2025-01-27 17:27:45 +0530

    27/01/2025

---
+++ Concepts/loop.js
@@ -0,0 +1,64 @@
+//simple loop for 5 time hello world print
+
+// for(let i = 0;i<5;i++){
+//     console.log("Hello World");
+// }
+
+
+//infinite loop
+
+// for(let i = 1;i>0;i++){
+//     console.log("Hello World", i);
+// }
+
+// while loop
+//  var i = 1;
+// while(i<=5){
+//     console.log("Hello World", i);
+//     i++;
+// }
+
+
+// do while loop
+
+// var i = 100;
+// do{
+//     console.log("Hello World from do while loop", i);
+//     i++;
+// }
+// while(i<=5);
+
+
+//for of loop
+
+//string and array each value to itreate krne ke liye use krta hai for of loop
+// let i = 0;
+// strValue = "HelloWorld";
+// for(let val of strValue){
+//     console.log(val);
+//     i++;
+    
+// }
+// console.log("Length of string is: ", i);
+
+
+
+// for in loop
+
+// let student= {
+//     name:"abhishek",
+//     class: "B.tech",
+//     rollno: 1234,
+//     pass: true
+// }
+
+//isme i key return kr rha hai name class rollno wagera
+//and value ke liye student[i] use kr rhe hai stuent[i] se value return ho rhi hai
+
+// for(let i in student){
+//     console.log("key",i, "value", student[i]);
+// }
+
+//__________________________________________________________________________________________________
+
+
---
+++ Concepts/practice-of-loop.js
@@ -0,0 +1,43 @@
+//print all the even number from 0 to 100
+
+// for(let i=0;i<=100;i++){
+//    if(i%2==0){
+//     console.log(i, "is even number");
+//    }
+// }
+
+
+//guess game number
+
+// const readline = require('node:readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+//yeh uppar ka sab terminal mai input le rha hai
+
+//neche wala logic hai bs guess the number game ka
+// let guessNumberValue = 25;
+// function guessNumber(){
+    
+//     rl.question(`Enter your number`, userNumber => {
+//         if(Number(userNumber)== guessNumberValue){
+//             console.log("Congratulation you have guess the correct number");
+//             rl.close();
+//         }
+//         else{
+//             guessNumber();
+//         }
+     
+//     });
+   
+   
+    
+// }
+
+// guessNumber();
+
+
+
