990c2b28497a0dde21101029bf2c88821f3e36dd
exclusiveabhi <imexclusiveabhi@gmail.com>
2025-01-27 21:45:36 +0530

    27/01/2025

---
+++ Concepts/strings.js
@@ -0,0 +1,54 @@
+//strings
+
+// let str = "abhishek";
+// console.log(str,"String length is",str.length);
+
+//template literals in ES6
+
+// let sentence =  `this is a random sentence`;
+// console.log(sentence);
+// console.log(sentence.length);
+// console.log(typeof(sentence));
+
+//new line print
+
+// let string = `this \nis \na \nstring`;
+// console.log(string);
+
+
+//string methods
+
+// let str = "Abhishek is good boy";
+// let upperCaseStr = str.toUpperCase();
+// console.log(upperCaseStr);
+
+//trim method
+// let str = "                           Abhishek is good boy";
+// let upperCaseStr = str.trim();
+// console.log(upperCaseStr);
+
+//slice method
+
+// let string = "AbhishekRajpoot"
+
+// let sliceString = string.slice(8,15)
+
+// console.log(sliceString)
+
+//concat method
+
+// let str1 = "this is first sentence";
+// let str2 = "this is secont sentence";
+
+// let combineStr = str1.concat(" ",str2)
+
+// console.log(combineStr)
+
+//replace methods
+
+// let value = "Abhishek is a good boy";
+// let newVal = "bad";
+
+// let afterReplace = value.replace("good", newVal)
+
+// console.log(afterReplace)
